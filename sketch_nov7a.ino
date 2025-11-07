f04b61c94c7895474b24131850b6f7f9e772f3e2
wube01 <120652742+wube01@users.noreply.github.com>
2025-11-07 13:21:17 +0200

    Update sketch_nov7a.ino

--- sketch_nov7a.ino
+++ sketch_nov7a.ino
@@ -11,15 +11,20 @@ void loop() {
   loops++;
   digitalWrite(13, HIGH);
   delay(500);
+  
   digitalWrite(12, HIGH);
   delay(500);
+  
   digitalWrite(11, HIGH);
   Serial.println("blink all on");
   delay(1000);
+  
   digitalWrite(MINU_LED, LOW);
   delay(500);
+  
   digitalWrite(12, LOW);
   delay(500);
+  
   digitalWrite(11, LOW);
   Serial.println("blink all off");
   Serial.println(loops);
